108cc6e75428ff71418b6aa4892038e5eed2812b
Atharv Nagpure <77281706+nagpureatharv@users.noreply.github.com>
2022-01-23 23:19:01 +0530

    Add files via upload

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+void Quicksort(int arr[] , int l , int h);
+
+int partation(int arr[] , int l , int h);
+
+void swap(int arr[] , int i , int j );
+
+int main()
+{
+  int n ;
+  cout<<"Enter the size of the array : ";
+  cin>>n;
+  int arr[n];
+  for(int i=0 ;i < n ; i++)
+  {
+    cout<<"Enter the number : ";
+    cin>>arr[i];
+  }
+  cout<<"Array brfore sorting : "<<endl;
+  for(int i=0 ;i < n ; i++)
+  {
+      
+   cout<<arr[i]<<"  ";
+  }
+  cout<<endl;
+
+
+  int l =0  ;
+  int h = n ;
+
+  Quicksort(arr,l,h);
+
+  cout<<"Array After sorting : "<<endl;
+
+  for(int i=0 ;i < n ; i++)
+  {
+    cout<<arr[i]<<"  ";
+  }
+
+}
+
+void Quicksort(int arr[] , int l , int h)
+{
+  if(l<h){
+    int pivot = partation(arr , l , h );
+    Quicksort(arr , l , pivot-1);
+    Quicksort(arr,pivot+1,h);
+  }
+
+
+}
+
+
+int partation(int arr[] , int l , int h)
+{
+
+  int pi = arr[l];
+  int i = l ;
+  int j = h ;
+ while(i<j){
+  while(arr[i]<=pi)
+  {
+    i++;
+  }
+  while(arr[j]>pi)
+  {
+    j--;
+  }
+
+  if(i<j)
+  {
+    swap(arr,i ,j);
+  }
+ }
+  
+  swap(arr, l ,j);
+  return j ;
+
+}
+
+void swap(int arr[] , int i , int j )
+{
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
